e023f1f1daf9c05d41365f65b4a8dd287776351d
gabb <gabriela_b_ferreira@estudante.sesisenai.org.br>
2026-08-24 16:08:41 -0300

    add- adicionei  a cadastro de animais e clientes

--- db.sql
+++ db.sql
@@ -1,1 +1,17 @@
-CREATE DATABASE 
+CREATE DATABASE aumigos;
+USE aumigos;
+
+CREATE TABLE clientes (
+    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL
+    nome VARCHAR (50) NOT NULL,
+    email VARCHAR (100) NOT NULL
+);
+CREATE TABLE animais (
+    id INT AUTO_INCREMENT
+    nome VARCHAR (50) NOT NULL,
+    especie VARCHAR (50) NOT NULL,
+    raca VARCHAR (50) NOT NULL,
+    idade INT NOT NULL,
+    cliente_id INT 
+    FOREIGN KEY (cliente_id) REFERENCES clientes (id)
+);
